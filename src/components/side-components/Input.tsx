d395a509b2a46c6bf479428d495aea82ebe3e301
talialy <allysonnomuraga@gmail.com>
2022-06-03 15:27:38 -0500

    rebase

--- src/components/side-components/Input.tsx
+++ src/components/side-components/Input.tsx
@@ -1,11 +1,11 @@
-import { forwardRef, HTMLInputTypeAttribute } from "react";
+import { forwardRef } from "react";
 
 interface Props {
     placeholder: string,
     required: boolean,
 }
 
-const Input = forwardRef((props: Props, ref) => {
+const Input = forwardRef((props: Props, ref:any) => {
     if (props.required) {
         return (
             <input type="text" ref={ref} className="input" placeholder={props.placeholder} required />
